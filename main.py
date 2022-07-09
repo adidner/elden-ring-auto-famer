b55d327b00a71a85f99f68d25133daa3731ddac0
Aaron Didner <adidner@gmail.com>
2022-07-09 11:00:30 -0400

    comment outline

---
+++ main.py
@@ -0,0 +1,49 @@
+print("hello world")
+
+
+map_key = ""
+open_recent_maps_key = ""
+confirm_key = ""
+ash_of_war_key = "q"
+forward_key = "w"
+left_key = "a"
+right_key = "d"
+back_key = "s"
+mouse_turn_radius = ""
+
+
+# Start while resting at the palace ledge site of grace
+
+
+# Get up 
+
+
+# Walk in the right direction (sprint walk?)
+
+
+# Turn if necessary?
+
+
+
+# Press ash_of_war_key
+
+
+
+# wait
+
+
+
+# Press map key
+
+
+
+# Go back to most recent grace via key
+
+
+
+# wait 
+
+
+# loop
+
+
